d6ddf5927170ace3eda8a35128c6346ea077f0d7
Vineet Karwa <vineetkarwa123@gmail.com>
2023-10-24 18:55:03 +0530

    Fix API to place order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -9,15 +9,14 @@ orderRouter.post(
     '/',
     isAuth,
     expressAsyncHandler(async (req, res) => {
-        const { body } = req.body
         const newOrder = new Order({
-            orderItems: body.orderItems.map((x) => ({ ...x, product: x._id })),
-            shippingAddress: body.shippingAddress,
-            paymentMethod: body.paymentMethod,
-            itemsPrice: body.itemsPrice,
-            shippingPrice: body.shippingPrice,
-            taxPrice: body.taxPrice,
-            totalPrice: body.totalPrice,
+            orderItems: req.body.orderItems.map((x) => ({ ...x, product: x._id })),
+            shippingAddress: req.body.shippingAddress,
+            paymentMethod: req.body.paymentMethod,
+            itemsPrice: req.body.itemsPrice,
+            shippingPrice: req.body.shippingPrice,
+            taxPrice: req.body.taxPrice,
+            totalPrice: req.body.totalPrice,
             user: req.user._id,
         });
 
